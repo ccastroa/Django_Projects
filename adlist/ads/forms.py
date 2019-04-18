fce4242f541be9d59fb998f5ec5fa7b44f076062
Carol Castro <ccastroa@umich.edu>
2019-04-18 18:44:08 +0000

    ad milestone 3

--- adlist/ads/forms.py
+++ adlist/ads/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from pics.models import Pic
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from pics.humanize import naturalsize
+from django.core.exceptions import ValidationError
+from django.core import validators
+
 
 # https://docs.djangoproject.com/en/2.1/topics/http/file-uploads/
 # https://stackoverflow.com/questions/2472422/django-file-upload-size-limit
@@ -16,31 +19,33 @@ class CreateForm(forms.ModelForm):
     # Call this 'picture' so it gets copied from the form to the in-memory model
     # It will not be the "bytes", it will be the "InMemoryUploadedFile"
     # because we need to pull out things like content_type
-    picture = forms.FileField(required=False, label='File to Upload <= '+max_upload_limit_text)
-    upload_field_name = 'picture'
+    ad = forms.FileField(required=False, label='File to Upload <= '+max_upload_limit_text)
+    upload_field_name = 'ad'
+    comment = forms.CharField(required=True, max_length=500, min_length=3, strip=True)
+
 
     class Meta:
         model = Ad
-        fields = ['title', 'text', 'picture']  # Picture is manual
+        fields = ['title', 'text', 'ad', 'comment']  # Picture is manual
 
     # Validate the size of the picture
     def clean(self) :
         cleaned_data = super().clean()
-        ad = cleaned_data.get('picture')
+        ad = cleaned_data.get('ad')
         if ad is None : return
         if len(ad) > self.max_upload_limit:
-            self.add_error('picture', "File must be < "+self.max_upload_limit_text+" bytes")
+            self.add_error('ad', "File must be < "+self.max_upload_limit_text+" bytes")
             
     # Convert uploaded File object to a picture
     def save(self, commit=True) :
         instance = super(CreateForm, self).save(commit=False)
 
         # We only need to adjust picture if it is a freshly uploaded file
-        f = instance.picture   # Make a copy
+        f = instance.ad   # Make a copy
         if isinstance(f, InMemoryUploadedFile):  # Extract data from the form to the model
             bytearr = f.read();
             instance.content_type = f.content_type
-            instance.picture = bytearr  # Overwrite with the actual image data
+            instance.ad = bytearr  # Overwrite with the actual image data
 
         if commit:
             instance.save()
--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -1,9 +1,10 @@
-from owner.models import Ad
+from owner.models import Ad, Comment
 
 from django.views import View
 from django.views import generic
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 from owner.util import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 
@@ -11,9 +12,27 @@ class AdListView(OwnerListView):
     model = Ad
     template_name = "ad_list.html"
 
+    def get(self, request) :
+        ad_list = Ad.objects.all()
+        favorites = list()
+        if request.user.is_authenticated:
+            # rows = [{'id': 2}]  (A list of rows)
+            rows = request.user.favorite_ads.values('id')
+            favorites = [ row['id'] for row in rows ]
+        ctx = {'ad_list' : thing_list, 'favorites': favorites}
+        return render(request, self.template_name, ctx)
+
+
 class AdDetailView(OwnerDetailView):
     model = Ad
     template_name = "ad_detail.html"
+    def get(self, request, pk) :
+        forum = Forum.objects.get(id=pk)
+        comments = Comment.objects.filter(forum=forum).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = { 'forum' : forum, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
+
 
 class AdCreateView((LoginRequiredMixin, View):
     template = 'ads/form.html'
@@ -70,3 +89,32 @@ def stream_file(request, pk) :
     response['Content-Length'] = len(ad.picture)
     response.write(ad.picture)
     return response
+
+
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.db.utils import IntegrityError
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Add PK",pk)
+        t = get_object_or_404(Thing, id=pk)
+        fav = Fav(user=request.user, ad=t)
+        try:
+            fav.save()  # In case of duplicate key
+        except IntegrityError as e:
+            pass
+        return HttpResponse()
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DeleteFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Delete PK",pk)
+        t = get_object_or_404(Thing, id=pk)
+        try:
+            fav = Fav.objects.get(user=request.user, ad=t).delete()
+        except Fav.DoesNotExist as e:
+            pass
+
+        return HttpResponse()
